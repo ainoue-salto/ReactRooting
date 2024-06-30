02eb89a1d49b37bff7668ca316697da3d181990b
ainoue-salto <ayaka.inoue@salto.link>
2024-06-30 14:58:28 +0900

    react rooting

--- react_rooting/src/App.js
+++ react_rooting/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import About from './component/About';
+import Home from './component/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react_rooting/src/component/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>このサイトは日記を投稿できるサイトです。</h2>
+      </div>
+    );
+  }
+}
+
+export default About;
